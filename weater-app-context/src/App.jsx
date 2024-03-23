f77aa6512373206d19885cd2a595c6252b89e345
kaliberda97 <kaliberdavictoria97@.gmail.com>
2024-03-23 21:49:02 +0100

    edit app.jsx

--- weater-app-context/src/App.jsx
+++ weater-app-context/src/App.jsx
@@ -1,13 +1,20 @@
-import './App.css'
+import "./App.css";
+import Form from "./components/Form";
+import Weather from "./components/Weather";
+import {CityProvider} from './CityContext';
 
-function App() {
- 
 
+
+const App = () => {
   return (
-    <>
-    
-    </>
-  )
-}
+    <CityProvider>
+      <div className="h-screen bg-cover bg-center opacity-70 " style={{backgroundImage:`url(https://t4.ftcdn.net/jpg/03/48/58/83/360_F_348588333_yEOn7lnklwAcNVSWf3iHUpFqmDgyOvCD.jpg)`}}>
+        <Form />
+        <Weather />
+      
+      </div>
+    </CityProvider>
+  );
+};
 
-export default App
+export default App;
